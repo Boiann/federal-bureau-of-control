42fa93f0fbd5cbc9dccf75fcde22bc22c58054bf
Bojan Bakaric <bakaricbojan@gmail.com>
2023-04-26 12:34:12 +0000

    Make migrations to database

--- fbd_app/models.py
+++ fbd_app/models.py
@@ -27,7 +27,7 @@ class Event(models.Model):
 
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    post = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
