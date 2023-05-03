098111a9547e73be18c72f5b9fe8f78d2e71095e
Bojan Bakaric <bakaricbojan@gmail.com>
2023-05-03 06:36:06 +0000

    Wire up Event and Comments with Admin

--- fbd_app/admin.py
+++ fbd_app/admin.py
@@ -1,9 +1,29 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 
 @admin.register(Event)
-class PostAdmin(SummernoteModelAdmin):
+class EventAdmin(SummernoteModelAdmin):
 
-    summernote_fields = ('content')
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    actions = ['approve_events']
+
+    def approve_events(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
