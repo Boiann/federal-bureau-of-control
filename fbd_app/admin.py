6920b08a46bf4d8a5e3da9204fed79e2615e4f55
Bojan Bakaric <114668562+Boiann@users.noreply.github.com>
2023-05-03 08:45:56 +0100

    Admin setup (#76)
    * Install Summernote and migrate
    
    * Wire up Event and Comments with Admin
    
    * Create second superuser/admin and wire up url

--- fbd_app/admin.py
+++ fbd_app/admin.py
@@ -1,5 +1,42 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 
-admin.register(Event)
+@admin.register(Event)
+class EventAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'status', 'created_on')
+    search_fields = ['title', 'content']
+    list_filter = ('status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('content',)
+    actions = ['approve_events']
+
+    def approve_events(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'body', 'post', 'created_on', 'approved')
+    list_filter = ('approved', 'created_on')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+class AssessorAdminSite(admin.AdminSite):
+    site_header = "Assessor admin"
+    site_title = "Assessor Admin Portal"
+    index_title = "Welcome to Federal Bureau of Control"
+
+
+assessor_admin_site = AssessorAdminSite(name='assessor-admin')
+
+
+assessor_admin_site.register(Event)
+assessor_admin_site.register(Comment)
--- fbd_project/urls.py
+++ fbd_project/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from fbd_app.admin import assessor_admin_site
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('assessor-admin/', assessor_admin_site.urls),
+    path('summernote/', include('django_summernote.urls')),
+
 ]
