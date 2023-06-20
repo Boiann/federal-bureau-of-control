51ae1b4048f0dca2a16e0407368b97574b696b56
Bojan Bakaric <bakaricbojan@gmail.com>
2023-06-20 19:42:52 +0000

    Add post method to views and comment message

--- fbd_app/views.py
+++ fbd_app/views.py
@@ -27,7 +27,39 @@ class EventDetail(View):
             {
                 "event": event,
                 "comments": comments,
+                "commented": False,
                 "liked": liked,
                 "comment_form": CommentForm(),
+            },
+        )
+
+    def post(self, request, slug, *args, **kwargs):
+
+        queryset = Event.objects.filter(status=1)
+        event = get_object_or_404(queryset, slug=slug)
+        comments = event.comments.filter(approved=True).order_by("-created_on")
+        liked = False
+        if event.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.post = event
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+        return render(
+            request,
+            "event-detail.html",
+            {
+                "event": event,
+                "comments": comments,
+                "commented": True,
+                "comment_form": CommentForm(),
+                "liked": liked
             },
         )
