83db3a8449a94ccb62c53bbab76faca73aafa1e1
Bojan Bakaric <bakaricbojan@gmail.com>
2023-07-06 19:13:31 +0000

    Debug set to false to see the deployment on heroku

--- fbd_project/settings.py
+++ fbd_project/settings.py
@@ -29,13 +29,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'federal-bureau-of-control.herokuapp.com',
     'localhost',
     '127.0.0.1',
-    '8000-boiann-federalbureauofc-0239egeoc1u.ws-eu101.gitpod.io',
+    '8000-boiann-federalbureauofc-cefpb3nwrvh.ws-eu101.gitpod.io',
 ]
 
 
